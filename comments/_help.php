ec2e5558e750843b7b0f90e706946eccf4884426
Pravinkumar <pravinkumarsaminathan@gmail.com>
2024-08-11 10:58:07 +0530

    modified help

---
+++ comments/_help.php
@@ -0,0 +1,13 @@
+<?php
+$info  = "<div style=\"color: #b3b9c5;\">Available comments</span>\n\n";
+echo $info;
+echo "<span style=\"color: rgb(5, 206, 145);\">help</span>    | List all available commands            | help\n";
+echo "<span style=\"color: rgb(5, 206, 145);\">about</span>   | About Me.                              | about\n";
+echo "<span style=\"color: rgb(5, 206, 145);\">info</span>    | Information about me.                  | info\n";
+echo "<span style=\"color: rgb(5, 206, 145);\">project</span> | Projects I have worked on.             | projects\n";
+echo "<span style=\"color: rgb(5, 206, 145);\">links</span>   | Get all my important links and socials | links\n";
+echo "<span style=\"color: rgb(5, 206, 145);\">skills</span>  | Display programming skills.            | skills\n";
+echo "<span style=\"color: rgb(5, 206, 145);\">email</span>   | Send an email to me.                   | email\n";
+echo "<span style=\"color: rgb(5, 206, 145);\">welcome</span> | display hero section.                  | welcome\n";
+echo "<span style=\"color: rgb(5, 206, 145);\">clear</span>   | Clear the screen.                      | clear\n";
+echo "<span style=\"color: #b3b9c5;\">---</div>";
