06ec5b7cab6020010432418b40095f2d33bbfd5d
Pravinkumar <pravinkumarsaminathan@gmail.com>
2024-08-11 14:50:34 +0530

    small chanage in help

--- comments/_help.php
+++ comments/_help.php
@@ -12,8 +12,8 @@ echo "<span style=\"color: rgb(5, 206, 145);\">welcome</span> | display hero sec
 echo "<span style=\"color: rgb(5, 206, 145);\">contact</span> | Display contact information.           | contact\n";
 echo "<span style=\"color: rgb(5, 206, 145);\">source</span>  | Open the GitHub repository.            | source\n";
 echo "<span style=\"color: rgb(5, 206, 145);\">github</span>  | Redirects to my GitHub account.        | github\n";
-echo "<span style=\"color: rgb(5, 206, 145);\">exit</span>    | terminate a page.                      | exit\n";
-echo "<span style=\"color: rgb(5, 206, 145);\">clear</span>   | Clear the screen.                      | clear\n\n";
+echo "<span style=\"color: rgb(5, 206, 145);\">clear</span>   | Clear the screen.                      | clear\n";
+echo "<span style=\"color: rgb(5, 206, 145);\">exit</span>    | terminate a page.                      | exit\n\n";
 echo "<span style=\"color: rgb(203, 213, 225);\">Tab or Ctrl + i        => autocompletes the command\n";
 echo "<span style=\"color: rgb(203, 213, 225);\">Up Arrow or Down Arrow => scroll through the output.\n";
 echo "<span style=\"color: #b3b9c5;\">---</div>";
