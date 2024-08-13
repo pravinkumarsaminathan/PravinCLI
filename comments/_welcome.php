f30a1e463053c7bafdfe8ad37afd5e464c3a202a
PRAVIN KUMAR <143002664+pravinkumarsaminathan@users.noreply.github.com>
2024-08-13 22:50:27 +0530

    Update _welcome.php

--- comments/_welcome.php
+++ comments/_welcome.php
@@ -4,9 +4,9 @@ $isSplitScreen = false;
 $isSmallSplitScreen = false;
 
 // Check if the screen width and height meet the criteria for a full-screen laptop
-if ($_SERVER['HTTP_USER_AGENT'] && preg_match('/(?i)msie [1-8]/',$_SERVER['HTTP_USER_AGENT']) === 0) {
-    $isLaptop = true;
-}
+// if ($_SERVER['HTTP_USER_AGENT'] && preg_match('/(?i)msie [1-8]/',$_SERVER['HTTP_USER_AGENT']) === 0) {
+//     $isLaptop = true;
+// }
 if (isset($_COOKIE['isSplitScreen'])) {
     if ($_COOKIE['isSplitScreen'] === '1') {
         $isSplitScreen = true;
@@ -26,8 +26,7 @@ echo '<div class="command-output">
 ██      ██   ██ ██   ██  ██  ██  ██ ██  ██ ██ ██  ██  ██    ██ ██  ██  ██ ██   ██ ██   ██                       7####^`^"7W7^"@####
 ██      ██   ██ ██   ██   ████   ██ ██   ████ ██   ██  ██████  ██      ██ ██   ██ ██   ██                       @#@b`         ^@#@^
                                                                                                                  ##^,,,,   ,,,,^#^
-                                                                                                                ,,@######"#######=                                                                                  
-</div>';
+                                                                                                                ,,@######"#######=</div>';
 echo "<div class=\"command-output\">I’m currently studying Computer Science and Engineering,                                                         .''555\"` '5555b|</div>";
 echo "<div class=\"command-output\">and I love diving into new coding challenges.                                                                    T\"@  ,,,^,mg,@,*</div>";
 echo "<div class=\"command-output\">                                                                                                                    %p||`~~'.#`</div>";
@@ -39,7 +38,7 @@ echo "<div class=\"command-output\">I designed this website to resemble a termin
 echo "<div class=\"command-output\">I have a deep love for the command line interface (CLI).                                                   C      3 * @#######Nl      `</div>";
 echo "<div class=\"command-output\">---                                                                                                        '            ^@##b     ($    !</div>";
 echo '<div class="command-output">Type \'help\' to see a list of commands.</div>';
-} elseif ($isLaptop && $isSmallSplitScreen) { 
+} elseif ($isSmallSplitScreen) { 
 echo '<div class="output" id="output">';
 echo '<div class="command-output">I’m currently studying Computer Science and Engineering,</div>';
 echo '<div class="command-output">and I love diving into new coding challenges.</div>';
